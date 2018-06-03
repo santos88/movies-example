594daaeacf7b949d09138217da287cab6b73c372
Santos Ramon <santos.ramon.brito@gmail.com>
2018-06-03 12:45:03 -0400

    Added Basic Structure without logic

---
+++ MoviesApp/MoviesApp/Controllers/MoviesListController.swift
@@ -0,0 +1,9 @@
+//
+//  MoviesListController.swift
+//  MoviesApp
+//
+//  Created by Santos Ramon on 6/3/18.
+//  Copyright © 2018 Santos Ramon. All rights reserved.
+//
+
+import Foundation
---
+++ MoviesApp/MoviesApp/Models/MovieModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieModel.swift
+//  MoviesApp
+//
+//  Created by Santos Ramon on 6/3/18.
+//  Copyright © 2018 Santos Ramon. All rights reserved.
+//
+
+import Foundation
